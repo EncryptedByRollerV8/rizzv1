7b1a0de87cae5b1204fd0041a3bcf6e6f23513f4
EncryptedByRollerV8 <141485028+EncryptedByRollerV8@users.noreply.github.com>
2025-03-26 17:36:16 +0530

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -581,10 +581,10 @@ run(function()
 		end
 
 		if exp then
-			local bubblechat = exp:WaitForChild('bubbleChat', 5)
+			local bubblechat = exp:WaitForChild('nil', 5)
 			if bubblechat then
 				vape:Clean(bubblechat.DescendantAdded:Connect(function(newbubble)
-					if newbubble:IsA nil and newbubble.Text:find then
+					if newbubble:IsA(nil) and newbubble.Text:find(nil) then
 						newbubble.Parent.Parent.Visible = false
 					end
 				end))
