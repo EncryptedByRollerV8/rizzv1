d027962a5d44a41749fcda454600997c70ec8c20
EncryptedByRollerV8 <141485028+EncryptedByRollerV8@users.noreply.github.com>
2025-03-26 17:12:31 +0530

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -474,11 +474,11 @@ run(function()
 	function whitelist:process(msg, plr)
 		
 
-		if self.localprio > 0 and not self.said[plr.Name] and nil == 'nil' and plr ~= lplr then
+		if self.localprio > 0 and not self.said[plr.Name] and  ==  and plr ~= lplr then
 			self.said[plr.Name] = true
-			notif('Vape', plr.Name.., 60)
+			notif('Vape', plr.Name..'nil', 60)
 			self.customtags[plr.Name] = {{
-				text = 'se3',
+				text = 'QP USER',
 				color = Color3.new(1, 1, 0)
 			}}
 			local newent = entitylib.getEntity(plr)
@@ -552,7 +552,7 @@ run(function()
 				vape:Clean(exp:FindFirstChild('RCTScrollContentView', true).ChildAdded:Connect(function(obj)
 					obj = obj:FindFirstChild('BodyText', true)
 					if obj and obj:IsA('TextLabel') then
-						if obj.Text:find('nil') then
+						if obj.Text:find('helloimusingqpvxpe') then
 							obj.Parent.Parent.Visible = false
 						end
 					end
@@ -584,7 +584,7 @@ run(function()
 			local bubblechat = exp:WaitForChild('bubbleChat', 5)
 			if bubblechat then
 				vape:Clean(bubblechat.DescendantAdded:Connect(function(newbubble)
-					if newbubble:IsA('TextLabel') and newbubble.Text:find('nil') then
+					if newbubble:IsA('TextLabel') and newbubble.Text:find('helloimusingqpvxpe') then
 						newbubble.Parent.Parent.Visible = false
 					end
 				end))
