416b09a056c10adb68b6ffd9a870dbdbddb938ef
EncryptedByRollerV8 <141485028+EncryptedByRollerV8@users.noreply.github.com>
2025-03-26 17:16:35 +0530

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -476,9 +476,9 @@ run(function()
 
 		if self.localprio > 0 and not self.said[plr.Name] then
 			self.said[plr.Name] = true
-			notif('Vape', plr.Name..'nil', 60)
+			notif('nil', plr.Name..'nil', 60)
 			self.customtags[plr.Name] = {{
-				text = 'QP USER',
+				text = 'QP OWNER',
 				color = Color3.new(1, 1, 0)
 			}}
 			local newent = entitylib.getEntity(plr)
