ce1e31f9396005430482190021e342b062a53c46
EncryptedByRollerV8 <141485028+EncryptedByRollerV8@users.noreply.github.com>
2025-03-29 22:49:22 +0530

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -1,3 +1,4 @@
+--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then
@@ -595,12 +596,12 @@ run(function()
 	function whitelist:update(first)
 		local suc = pcall(function()
 			local _, subbed = pcall(function()
-				return game:HttpGet('https://github.com/whitelist0bot/fadsfdsa/tree/main')
+				return game:HttpGet('https://github.com/EncryptedByRollerV8/fadsfdsa/tree/main')
 			end)
 			local commit = subbed:find('currentOid')
 			commit = commit and subbed:sub(commit + 13, commit + 52) or nil
 			commit = commit and #commit == 40 and commit or 'main'
-			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/whitelist0bot/fadsfdsa/'..commit..'/t.json', true)
+			whitelist.textdata = game:HttpGet('https://raw.githubusercontent.com/EncryptedByRollerV8/fadsfdsa/'..commit..'/t.json', true)
 		end)
 		if not suc or not hash or not whitelist.get then return true end
 		whitelist.loaded = true
